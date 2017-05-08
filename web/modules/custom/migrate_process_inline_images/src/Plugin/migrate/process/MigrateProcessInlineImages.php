21c1ef2fc9ff9b1e5260419ea8e573c626e97a57
Greg Anderson <greg.1.anderson@greenknowe.org>
2017-05-07 23:11:06 -0700

    Processs all of the images in each body in migrate_process_inline_images, not just the first.

--- web/modules/custom/migrate_process_inline_images/src/Plugin/migrate/process/MigrateProcessInlineImages.php
+++ web/modules/custom/migrate_process_inline_images/src/Plugin/migrate/process/MigrateProcessInlineImages.php
@@ -44,36 +44,39 @@ class MigrateProcessInlineImages extends ProcessPluginBase {
     $file_storage = \Drupal::entityTypeManager()->getStorage('file');
 
     // Pull the image path out of the image element.
-    if (!preg_match('#src="([^"]*)"#', $imgElement, $matches)) {
+    if (!preg_match_all('#src="([^"]*)"#', $imgElement, $matches, PREG_SET_ORDER)) {
         return $imgElement;
     }
-    $imagePath = $matches[1];
 
-    // If the image is stored in 'public://image', then an img src of
-    // 'files/image/subdir/pict.jpg' will correspond to an entity uri
-    // of 'subdir/pict.jpg'. The 'base' in this example is 'files/image';
-    // strip the base off the img src so that we can use it to search for
-    // the file by its entity uri.
-    $imageBase = isset($this->configuration['base']) ? $this->configuration['base'] : 'sites/default/files';
-    $imageBase = rtrim($imageBase, '/');
-    $imagePath = str_replace('/' . $imageBase . '/', '', $imagePath);
+    foreach ($matches as $match) {
+        $imagePath = $match[1];
 
-    $fids = \Drupal::entityQuery('file')
-      ->condition('uri', '%' . $imagePath . '%', 'LIKE')
-      ->range(0, 2)
-      ->execute();
+        // If the image is stored in 'public://image', then an img src of
+        // 'files/image/subdir/pict.jpg' will correspond to an entity uri
+        // of 'subdir/pict.jpg'. The 'base' in this example is 'files/image';
+        // strip the base off the img src so that we can use it to search for
+        // the file by its entity uri.
+        $imageBase = isset($this->configuration['base']) ? $this->configuration['base'] : 'sites/default/files';
+        $imageBase = rtrim($imageBase, '/');
+        $imagePath = str_replace('/' . $imageBase . '/', '', $imagePath);
 
-    if (!$fids) {
-        print "No image found for path: $imagePath (was " . $matches[1] . ")\n";
-        return $imgElement;
+        $fids = \Drupal::entityQuery('file')
+            ->condition('uri', '%' . $imagePath . '%', 'LIKE')
+            ->range(0, 2)
+            ->execute();
+
+        if (!$fids) {
+            print "No image found for path: $imagePath (was " . $matches[1] . ")\n";
+            return $imgElement;
+        }
+        $files = $file_storage->loadMultiple($fids);
+        $firstFile = reset($files);
+        $uuid = $firstFile->uuid();
+
+        $align = $this->determinAlign($imgElement);
+        $imgElement = str_replace('<img ', "<img$align data-entity-type=\"file\" data-entity-uuid=\"$uuid\" ",
+            $imgElement);
     }
-    $files = $file_storage->loadMultiple($fids);
-    $firstFile = reset($files);
-    $uuid = $firstFile->uuid();
-
-    $align = $this->determinAlign($imgElement);
-    $imgElement = str_replace('<img ', "<img$align data-entity-type=\"file\" data-entity-uuid=\"$uuid\" ", $imgElement);
-
     return $imgElement;
   }
 
