1b561a9c9d8506a7a83c232d5409a02ef7defefb
Greg Anderson <greg.1.anderson@greenknowe.org>
2019-03-13 12:34:01 -0700

    Drush 9.6.0-rc6

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -6,47 +6,15 @@
 $settings['container_yamls'][] = __DIR__ . '/services.yml';
 
 /**
- * Ensure that Twig files are correctly located.
- */
-if (isset($_ENV['PANTHEON_ROLLING_TMP']) && isset($_ENV['PANTHEON_DEPLOYMENT_IDENTIFIER'])) {
-  // Relocate the compiled twig files to <binding-dir>/tmp/ROLLING/twig.
-  // The location of ROLLING will change with every deploy.
-  $settings['php_storage']['twig']['directory'] = $_ENV['PANTHEON_ROLLING_TMP'];
-  // Ensure that the compiled twig templates will be rebuilt whenever the
-  // deployment identifier changes.  Note that a cache rebuild is also necessary.
-  $settings['deployment_identifier'] = $_ENV['PANTHEON_DEPLOYMENT_IDENTIFIER'];
-  $settings['php_storage']['twig']['secret'] = $_ENV['DRUPAL_HASH_SALT'] . $settings['deployment_identifier'];
-}
-
-/**
  * Include the Pantheon-specific settings file.
  *
  * n.b. The settings.pantheon.php file makes some changes
  *      that affect all envrionments that this site
  *      exists in.  Always include this file, even in
- *      a local development environment, to ensure that
+ *      a local development environment, to insure that
  *      the site settings remain consistent.
  */
 include __DIR__ . "/settings.pantheon.php";
-
-/**
- * Place the config directory outside of the Drupal root.
- */
-$config_directories = array(
-  CONFIG_SYNC_DIRECTORY => dirname(DRUPAL_ROOT) . '/config',
-);
-
-/**
- * Disable outgoing email on Pantheon, except on test and live environments.
- */
-if (
-  isset($_ENV['PANTHEON_ENVIRONMENT']) &&
-  ( ($_ENV['PANTHEON_ENVIRONMENT'] != 'live') && ($_ENV['PANTHEON_ENVIRONMENT'] != 'test') )
-) {
-  $conf['mail_system'] = array(
-    'default-system' => 'DevelMailLog',
-  );
-}
 
 /**
  * If there is a local settings file, then include it
@@ -55,17 +23,4 @@ $local_settings = __DIR__ . "/settings.local.php";
 if (file_exists($local_settings)) {
   include $local_settings;
 }
-
-/**
- * Always install the 'standard' profile to stop the installer from
- * modifying settings.php.
- *
- * See: tests/installer-features/installer.feature
- */
 $settings['install_profile'] = 'standard';
-
-
-# $settings['file_public_base_url'] = 'http://downloads.example.com/files';
-$settings['file_public_path'] = 'files';
-# $settings['file_private_path'] = '';
-
