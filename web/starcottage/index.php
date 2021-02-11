54849a097d1ad8162aea54275fcf1e8c800a8276
Greg Anderson <greg.1.anderson@greenknowe.org>
2021-02-10 21:05:01 -0800

    More formatting, a couple more pictures for star cottage page

---
+++ web/starcottage/index.php
@@ -0,0 +1,133 @@
+<html>
+	<head>
+		<title>Star Cottage</title>
+		<link rel="stylesheet" href="main.css">
+	</head>
+	<body>
+		<h1>Star Cottage</h1>
+
+		<p>
+		Coming Soon....
+		</p>
+
+		<ul>
+		  <li>Green Home with Passive Solar details and advanced building techniques.</li>
+		  <li>Off street parking +1 on street parking.</li>
+		  <li>Full basement with high ceiling would make a great work space or just stop paying $$$ every month for public storage!</li>
+		  <li>All Energy Star appliances</li>
+		  <li>Minisplit heat pump both heats in the winter and cools in the summer for perfect comfort year round</li>
+		  <li>Extra insulation</li>
+		  <li>Low VOC finishes throughout</li>
+		  <li>Built in air exchanger for clean, healthy air</li>
+		  <li>Large bathroom and open kitchen</li>
+		  <li>EV ready</li>
+		  <li>1.6 KWH solar array</li>
+		  <li>Large garden, ideal for growing food or just relaxing</li>
+		  <li>High performance windows, door and construction makes a comfortable space.</li>
+		  <li>Open floor plan adjusts to your needs</li>
+		  <li>Near public shuttle, SamTrans 292 and Highway 101</li>
+		  <li>Enjoy San Bruno Mountain and the bay, just minuets from down town SF.</li>
+		</ul>
+
+		<p>
+		Send inquires to brisbanerental@reetstreet.com
+		</p>
+
+		<p>
+		This newly built home has 11' ceilings and plenty of windows, giving you a wonderful view of San Bruno Mountain to the west.  The large overhangs keep the house shady and cool in the summer.  The roof sports a 1.6kwh solar array that is estimated to provide most of the household electric, PG&amp;E willing.
+		</p>
+
+		<p>
+		The floor plan is completely open, with a central partition wall so that you can configure your space and furnishing any way that you like.  Keep it open or create a space to your liking.  There is a large airy bathroom that includes ample lighting, exhaust fan, solar tube and a heat lamp for a luxurious bathing experience.  The kitchen is compact but with plenty of drawer and cabinet space including a few extra touches like a built-in spice rack.  All appliances are Energy Star rated including the new stainless steel induction stove and LED range hood.  You can eat informal meals at the kitchen bar with granite counter tops and pendant lighting. All of the lighting is LED including in the living area which is brightly lit with four rustic chandeliers.  Soft vinyl flooring is quiet and feels resilient under your feet. Beneath the floor and above the ceiling is a full 12” of insulation that will cut down on that cold and drafty feeling you sometimes get from cheap and old construction.
+		</p>
+
+		<p>
+		Extra attention was given to indoor air quality.   The house is tightly sealed but two exhaust fans plus a heat recovery ventilation system means there will always be fresh air to breath even in the winter when the house is closed up against the weather.  A ceiling fan makes sure everything is circulating.
+		</p>
+
+		<p>
+		The living space is compact, but the building includes a full basement with ample electrical service.  Stop paying for public storage or have your very own hobby space or both!  The basement has 12' ceilings, and a separate storage room, there is plenty of space to expand.  The house also has a small attic you can use for storing a few items out of sight long term.
+		</p>
+
+		<p>
+		Most small homes don't provide off-street parking.  This one not only has off street parking, but is EV ready.  You can park a second car across the driveway if you need an extra space.  The city free shuttle to Bart and Caltrain stops at the corner.  Brisbane is just 20 minutes from downtown SF. If you are heading south, being out of SF saves a lot of time while still being able to enjoy the city on the weekends.  Within walking distance to SamTrans 292, highly ranked Brisbane Elementary school, Lipman Middle School, the Community Park the local market and Ace Hardware. Enjoy hiking on San Bruno Mountain, relaxing at the local marina, and explore the Bay Trail. Don't forget Brisbane's exceptional weather! It's sunny in Brisbane even when it is fogged in all around us.
+		</p>
+
+		<p>
+		The unit was designed with accessibility in mind.  However, the unit is not fully ADA compliant.  The kitchen isnt large enough to accommodate a wheelchair. The vanity is the wrong type for wheelchair access.  
+		</p>
+
+		<h2>
+		Terms:
+		</h2>
+
+		<p>
+		Tenant to pay any electricity charges beyond what the solar array provides.  Tenant also pays water.  20 gallon garbage service is included with rent.  Pets require and extra $100 deposit, no large aquariums.  Tenant must provide proof of renter's insurance within 30 days of taking possession.  $2600/month.  Deposit $3900.   Must provide proof of employment and submit a credit report and references.  Washer and dryer provided in unit.
+		</p>
+
+		<h2>
+		Other stuff.  
+		</h2>
+
+		<p>
+		The construction was done with nearly 100% reclaimed lumber and the project was zero waste with the possible exception of the asphalt hauled off by the contractor during water and sewer connections.  All scrap lumber, cardboard, Styrofoam and metal was recycled or upcycled.  Concrete, soil and rock was recycled on site. Project includes two 4000 gallon rain water tanks.   
+		</p>
+<!--
+		<p>
+		This newly built home has 11' ceilings and plenty of windows, giving you a wonderful view of San Bruno Mountain to the west.  The large overhangs keep the house shady and cool in the summer.  The roof sports a 1.6kwh solar array that is estimated to provide most of the household electric needs.
+		</p>
+
+		<p>
+		The floorplan is completely open, with a central partition wall so that you can configure your space and furnishing any way that you like.  Keep it open, or create a space to your liking.  There is a large airy bathroom that includes ample lighting, exhaust fan, solar tube and a heat lamp and shower for a luxurious bathing experience.  The kitchen is compact but with plenty of drawer and cabinet space including a few extra touches like a built-in spice rack.  All appliances are Energy Star rated, including the new stainless steel induction stove and LED range hood.  You can eat informal meals at the kitchen bar with granite counter tops and pendant lighting. All of the lighting is LED including in the living area which is brightly lit with four rustic chandeliers.  Soft vinyl flooring is quiet and feels resilient under your feet. Beneath the floor and above the ceiling is a full 12” of insulation that will cut down on that cold and drafty feeling you sometimes get from cheap and old construction.
+		</p>
+
+		<p>
+		Extra attention was given to indoor air quality.   The house is tightly sealed, but two exhaust fans plus a heat recovery ventilation system means there will always be fresh air to breath even in the winter when the house is closed up against the weather.  A ceiling fan makes sure everything is circulating.
+		</p>
+
+		<p>
+		The living space is compact, but the building includes a full basement with ample electrical service.  Stop paying for public storage or have your very own hobby space, or both!  With 12' ceilings, and a separate storage room, there is plenty of space to expand.  The house also has a small attic you can use for storing a few items out of sight long term.
+		</p>
+
+		<p>
+		Most small homes don't provide off-street parking.  This one not only has off street parking, but is EV ready.  You can park a second car across the driveway if you need an extra space.  The city free shuttle to Bart and Caltrain stops at the corner.  Brisbane is just 20 minutes from downtown SF. If you are heading south, being out of SF saves a lot of time while still being able to enjoy the city on the weekends.  Within walking distance to SamTrans 292, highly ranked Brisbane Elementary school, Lipman Middle School, the Community Park the local market and Ace Hardware. Enjoy hiking on San Bruno Mountain, relaxing at the local marina, and explore the Bay Trail. Don't forget Brisbane's exceptional weather! It's sunny in Brisbane even when it is fogged in all around us.
+		</p>
+		
+		<h2>
+		Terms:
+		</h2>
+
+		<p>
+		Tenant to pay any electricity charges beyond what the solar array provides.  Tenant also pays water.  20 gallon garbage service is included with rent.  Pets are fine, no large aquariums.  Tenant must provide proof of renter's insurance within 30 days of taking possession.  $2600/month.  Deposit $3900.   Must provide proof of employment and submit a credit report and references.
+		<p>
+
+		<h2>
+		Contact:
+		</h2>
+
+		<p>
+		starcottage@greenknowe.org
+		</p>
+-->
+
+		<h2>
+		Pictures:
+		</h2>
+				
+		<a href="img/pic-entry.jpg" target="_blank"><img src="img/pic-entry.jpg" alt="Entry" class="narrow"/></a>
+		
+		<a href="img/pic-bath.jpg" target="_blank"><img src="img/pic-bath.jpg" alt="Bathroom" class="narrow"/></a>
+	
+		<a href="img/kitchen.jpg" target="_blank"><img src="img/pic-kitchen.jpg" alt="Kitchen" class="wide"/></a>
+			
+		<a href="img/pic-west.jpg" target="_blank"><img src="img/pic-west.jpg" alt="Wall" class="wide"/></a>
+			
+		<a href="img/pic-lights.jpg" target="_blank"><img src="img/pic-lights.jpg" alt="Lights" class="wide"/></a>
+
+		<a href="img/pic-ceiling.jpg" target="_blank"><img src="img/pic-ceiling.jpg" alt="Ceiling" class="wide"/></a>
+
+		<a href="img/pics-mountain.jpg" target="_blank"><img src="img/pics-mountain.jpg" alt="Mountains" class="wide"/></a>
+
+
+	</body>
+</html>
